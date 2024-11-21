5c2ea2e380bf6f74159f90dd10203ef76c4fd735
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2024-11-20 21:15:05 -0300

    feat: finalizando a página 3

--- pages/page3.lua
+++ pages/page3.lua
@@ -1,87 +1,265 @@
------------------------------------------------------------------------------------------
--- 
--- page3.lua
--- 
------------------------------------------------------------------------------------------
-
-local composer = require( "composer" )
+local composer = require("composer")
 local scene = composer.newScene()
 
-local background, nextPageButton, previousPageButton, contentText
+local background, nextPageButton, previousPageButton, contentText, instructionText, textBackground, titleText, sampleDna, dnaDetergent, animationStartButton, alcoholPipette, nextAnimationButton, dnaReagent, extractedDna
 
-local function onNextPageButtonTouch( self, event )
-	if event.phase == "ended" or event.phase == "cancelled" then
-		composer.gotoScene( "pages.page4", "slideLeft", 800 )
-		return true
-	end
+local animationStage = 0
+
+local function onNextPageButtonTouch(self, event)
+    if event.phase == "ended" or event.phase == "cancelled" then
+        composer.gotoScene("pages.page4", "slideLeft", 800)
+        return true
+    end
 end
 
-local function onPreviousPageButtonTouch( self, event )
-	if event.phase == "ended" or event.phase == "cancelled" then
-		composer.gotoScene( "pages.page2", "slideRight", 800 )
-		return true
-	end
+local function onPreviousPageButtonTouch(self, event)
+    if event.phase == "ended" or event.phase == "cancelled" then
+        composer.gotoScene("pages.page2", "slideRight", 800)
+        return true
+    end
 end
 
-function scene:create( event )
-	local sceneGroup = self.view
+local function onAnimationStartButtonTouch(event)
+    if event.phase == "ended" then
+        transition.to(animationStartButton, {
+            alpha = 0,
+            time = 500
+        })
 
-	background = display.newImageRect( sceneGroup, "assets/imgs/pageContentBg.png", display.contentWidth, display.contentHeight )
-	background.anchorX = 0
-	background.anchorY = 0
-	background.x, background.y = 0, 0
-
-	nextPageButton = display.newImageRect( sceneGroup, "assets/imgs/nextpagebutton.png", 87, 107 )
-	nextPageButton.x = 670
-	nextPageButton.y = 950
-
-	previousPageButton = display.newImageRect( sceneGroup, "assets/imgs/previousPageButton.png", 87, 100 )
-	previousPageButton.x = 90
-	previousPageButton.y = 944
-
-	contentText = display.newText( sceneGroup, "Página 4", display.contentCenterX, 210, "ComicNeue-Regular", 50 )
-	contentText:setFillColor( 0.165, 0.267, 0.365 )
-
-	sceneGroup:insert( nextPageButton )
-	sceneGroup:insert( previousPageButton )
+        transition.to(dnaDetergent, {
+            x = sampleDna.x,
+            y = sampleDna.y,
+            time = 1000,
+            onComplete = function()
+                transition.to(dnaDetergent, {
+                    alpha = 0,
+                    time = 500,
+                    onComplete = function()
+                        instructionText.text = "O álcool é adicionado na amostra com a pipeta para separar o DNA das outras substâncias"
+                        instructionText.size = 20
+                        nextAnimationButton.isVisible = true
+                        alcoholPipette.isVisible = true
+                        animationStage = 1
+                    end
+                })
+            end
+        })
+    end
+    return true
 end
 
-function scene:show( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if phase == "will" then
-	elseif phase == "did" then
-		nextPageButton.touch = onNextPageButtonTouch
-		nextPageButton:addEventListener( "touch", nextPageButton )
+local function onNextAnimationButtonTouch(event)
+    if event.phase == "ended" then
+        transition.to(nextAnimationButton, {
+            alpha = 0,
+            time = 500,
+            onComplete = function()
+                nextAnimationButton.isVisible = false
+            end
+        })
 
-		previousPageButton.touch = onPreviousPageButtonTouch
-		previousPageButton:addEventListener( "touch", previousPageButton )
-	end	
+        if animationStage == 1 then
+            transition.to(alcoholPipette, {
+                x = sampleDna.x,
+                y = sampleDna.y,
+                time = 1000,
+                onComplete = function()
+                    transition.to(alcoholPipette, {
+                        alpha = 0,
+                        time = 500,
+                        onComplete = function()
+                            alcoholPipette.isVisible = false
+                            alcoholPipette.alpha = 1
+
+                            instructionText.text = "É adicionado uma solução ao DNA para que ele seja dissolvido e utilizado no teste"
+                            nextAnimationButton.isVisible = true
+                            nextAnimationButton.alpha = 1
+                            dnaReagent.isVisible = true
+                            animationStage = 2
+                        end
+                    })
+                end
+            })
+        elseif animationStage == 2 then
+            transition.to(dnaReagent, {
+                x = sampleDna.x,
+                y = sampleDna.y,
+                time = 1000,
+                onComplete = function()
+                    transition.to(dnaReagent, {
+                        alpha = 0,
+                        time = 500,
+                        onComplete = function()
+                            dnaReagent.isVisible = false
+                            dnaReagent.alpha = 1
+
+                            transition.to(sampleDna, {
+                                x = display.contentCenterX,
+                                y = 770,
+                                time = 1000,
+                                onComplete = function()
+                                    animationStage = 3
+                                end
+                            })
+
+							instructionText.text = "Agite o dispositivo para misturar a solução e liberar o DNA purificado"
+							instructionText.size = 25
+                        end
+                    })
+                end
+            })
+        end
+    end
+    return true
 end
 
-function scene:hide( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if event.phase == "will" then
-		nextPageButton:removeEventListener( "touch", nextPageButton )
-		previousPageButton:removeEventListener( "touch", previousPageButton )
-	end
+local function onShake(event)
+    if animationStage == 3 then
+        transition.to(sampleDna, {
+            alpha = 0,
+            time = 500,
+            onComplete = function()
+                sampleDna.isVisible = false
+
+                extractedDna.alpha = 0
+                extractedDna.isVisible = true
+                transition.to(extractedDna, {
+                    alpha = 1,
+                    time = 500
+                })
+            end
+        })
+
+        animationStage = 4
+    end
 end
 
-function scene:destroy( event )
-	local sceneGroup = self.view
-	
+
+function scene:create(event)
+    local sceneGroup = self.view
+
+    background = display.newImageRect(sceneGroup, "assets/imgs/pageContentBg.png", display.contentWidth, display.contentHeight)
+    background.anchorX = 0
+    background.anchorY = 0
+    background.x, background.y = 0, 0
+
+    nextPageButton = display.newImageRect(sceneGroup, "assets/imgs/nextpagebutton.png", 87, 107)
+    nextPageButton.x = 670
+    nextPageButton.y = 950
+
+    previousPageButton = display.newImageRect(sceneGroup, "assets/imgs/previousPageButton.png", 87, 100)
+    previousPageButton.x = 90
+    previousPageButton.y = 944
+
+    instructionText = display.newText({
+        parent = sceneGroup,
+        text = "O detergente é adicionado à amostra para quebrar as paredes das células",
+        x = display.contentCenterX,
+        y = 58,
+        width = 300,
+        font = "assets/fonts/ComicNeue-Bold.ttf",
+        fontSize = 25,
+        align = "center"
+    })
+    instructionText:setFillColor(1, 1, 1)
+
+    titleText = display.newText(sceneGroup, "Extração do DNA", display.contentCenterX, 210, "assets/fonts/ComicNeue-Bold.ttf", 48)
+    titleText:setFillColor(0.165, 0.267, 0.365)
+
+    textBackground = display.newImageRect(sceneGroup, "assets/imgs/textBackground.png", display.contentWidth, 580)
+    textBackground.anchorX = 0.5
+    textBackground.anchorY = 0.5
+    textBackground.x = display.contentCenterX
+    textBackground.y = 590
+
+    contentText = display.newText({
+        parent = sceneGroup,
+        text = "Depois da coleta, as amostras são enviadas ao laboratório, onde o DNA é extraído das células. Esse processo envolve a quebra das células para liberar o material genético que será utilizado na análise. A qualidade do DNA extraído é essencial para garantir a precisão do teste.",
+        x = display.contentCenterX,
+        y = 480,
+        width = 689,
+        font = "assets/fonts/ComicNeue-Bold.ttf",
+        fontSize = 32,
+        align = "left"
+    })
+    contentText:setFillColor(0.165, 0.267, 0.365)
+
+    sampleDna = display.newImageRect(sceneGroup, "assets/imgs/pg3/sampleDna.png", 116.9, 213.5)
+    sampleDna.x = 180
+    sampleDna.y = 770
+
+    dnaDetergent = display.newImageRect(sceneGroup, "assets/imgs/pg3/dnaDetergent.png", 152.6, 170.1)
+    dnaDetergent.x = 575
+    dnaDetergent.y = 770
+
+    animationStartButton = display.newImageRect(sceneGroup, "assets/imgs/pg3/animationStartButton.png", 195.3, 38.5)
+    animationStartButton.x = display.contentCenterX
+    animationStartButton.y = 660
+
+    nextAnimationButton = display.newImageRect(sceneGroup, "assets/imgs/pg3/nextAnimationButton.png", 195.3, 38.5)
+    nextAnimationButton.x = display.contentCenterX
+    nextAnimationButton.y = 660
+    nextAnimationButton.isVisible = false
+
+    alcoholPipette = display.newImageRect(sceneGroup, "assets/imgs/pg3/alcoholPipette.png", 184.1, 242.9)
+    alcoholPipette.x = 575
+    alcoholPipette.y = 750
+    alcoholPipette.isVisible = false
+
+    dnaReagent = display.newImageRect(sceneGroup, "assets/imgs/pg3/dnaReagent.png", 239.4, 249.2)
+    dnaReagent.x = 575
+    dnaReagent.y = 750
+    dnaReagent.isVisible = false
+
+    extractedDna = display.newImageRect(sceneGroup, "assets/imgs/pg3/extractedDna.png", 199.5, 216.3)
+    extractedDna.x = display.contentCenterX
+    extractedDna.y = 770
+    extractedDna.isVisible = false
+
+    animationStartButton:addEventListener("touch", onAnimationStartButtonTouch)
+    nextAnimationButton:addEventListener("touch", onNextAnimationButtonTouch)
+
+    sceneGroup:insert(nextPageButton)
+    sceneGroup:insert(previousPageButton)
 end
 
----------------------------------------------------------------------------------
+function scene:show(event)
+    local sceneGroup = self.view
+    local phase = event.phase
 
-scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
-scene:addEventListener( "destroy", scene )
+    if phase == "will" then
+    elseif phase == "did" then
+        nextPageButton.touch = onNextPageButtonTouch
+        nextPageButton:addEventListener("touch", nextPageButton)
 
------------------------------------------------------------------------------------------
+        previousPageButton.touch = onPreviousPageButtonTouch
+        previousPageButton:addEventListener("touch", previousPageButton)
+
+        Runtime:addEventListener("accelerometer", onShake)
+    end
+end
+
+function scene:hide(event)
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    if event.phase == "will" then
+        nextPageButton:removeEventListener("touch", nextPageButton)
+        previousPageButton:removeEventListener("touch", previousPageButton)
+        animationStartButton:removeEventListener("touch", onAnimationStartButtonTouch)
+        nextAnimationButton:removeEventListener("touch", onNextAnimationButtonTouch)
+
+        Runtime:removeEventListener("accelerometer", onShake)
+    end
+end
+
+function scene:destroy(event)
+    local sceneGroup = self.view
+end
+
+scene:addEventListener("create", scene)
+scene:addEventListener("show", scene)
+scene:addEventListener("hide", scene)
+scene:addEventListener("destroy", scene)
 
 return scene
